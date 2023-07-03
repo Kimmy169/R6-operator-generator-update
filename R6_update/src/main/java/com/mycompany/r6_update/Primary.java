2e5e247044bcfdc40b7ca96f26440b05f1677217
Kimmy169 <83825284+Kimmy169@users.noreply.github.com>
2023-07-03 14:13:13 +0200

    update

--- R6_update/src/main/java/com/mycompany/r6_update/Primary.java
+++ R6_update/src/main/java/com/mycompany/r6_update/Primary.java
@@ -2,17 +2,14 @@ package com.mycompany.r6_update;
 
 public class Primary extends Weapon{
     
-    private  String[] sights = {"3.0X","2.5X","1.5X","1.0X","none"};
-    private  String[] barrels = {"muzzle break","extended barrel", "compensator", "","none"};
-    private  String[] grips = {"angled grip", "vertical grip","none"};
-    private  boolean underbarrel = true;
-    
-    public Primary(WeaponTypes weaponType, String name, int id,String[] sights,String[] barrels,String[] grips, boolean underbarrel) {
+    private  String[] sights;
+    private  String[] barrels;
+    private  String[] grips;
+    private  boolean underbarrel;
+
+    public Primary(WeaponTypes weaponType, String name, int id) {
         super(weaponType, name, id);
-        this.sights = sights;
-        this.barrels = barrels;
-        this.grips = grips;
-        this.underbarrel = underbarrel;
     }
+    
     
 }
